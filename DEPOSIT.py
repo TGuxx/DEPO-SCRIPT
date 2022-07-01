7075a8e43b4c3f074749103f9fd96dcfc4433b35
Gux <43382783+MAINcsgo@users.noreply.github.com>
2022-07-01 16:58:48 +0200

    Update DEPOSIT.py

--- DEPOSIT.py
+++ DEPOSIT.py
@@ -1,8 +1,8 @@
 from discordwebhook import Discord
 import pyautogui
 
-#DC WEBHOOK (Use your own webhook)
-WEBHOOK = "https://discord.com/api/webhooks/992425649740455937/Qa5IIsnaOHlG56IdfsB23JeoDF65-CKMIJ-18d_q87KI1J_MOwIQ3QAH0tmDqYesIWIF"
+#DC WEBHOOK
+WEBHOOK = "<INSERT YOUR WEBHOOKHERE>"
 
 #COLORS
 IAM_READY_COLOR = (0,199,77) #BRIGHT GREEN COLOR
@@ -13,7 +13,13 @@ CLICK_POS = (1050,750)
 #DISCORD NOTIFICATION
 def SEND_NOTIFICATION():
     discord = Discord(url=WEBHOOK)
-    discord.post(content="YOUR ITEM IS READY TO BE DELIVERED")
+    discord.post(
+        embeds=[
+            {
+                "title": "READY TO DELIVER",
+            }
+        ],
+    )
 
 #MAIN LOOP
 def main():
@@ -21,7 +27,7 @@ def main():
        #SCREENSHOT OF A SCREEN
        sc = pyautogui.screenshot();
 
-       #CHEKING PHASE
+       #CHECKING PHASE
        pixel_check = sc.getpixel(CLICK_POS)
        if pixel_check == (IAM_READY_COLOR):
            pyautogui.click(CLICK_POS)
